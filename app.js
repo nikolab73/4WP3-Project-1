f36da0acb35aea191a5132d384fac2a91f444c86
Nikola Bogdanovic <bogdanon@mcmaster.ca>
2026-02-27 10:40:34 -0500

    initializing db in model file

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
-const path = require('path');
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: false }));
-
-// Parse JSON data
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.engine('mustache', mustacheExpress());
 app.set('view engine', 'mustache');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', './views');
 
 const controller = require('./app.ctrl.js');
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-  console.log('Press Ctrl+C to stop the server');
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
---
+++ app.model.js
@@ -0,0 +1,24 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+const dbPath = path.join(__dirname, 'trips.db');
+const db = new sqlite3.Database(dbPath, (err) => {
+  if (err) {
+  } else {
+    initializeDatabase();
+  }
+});
+
+async function initializeDatabase() {
+    db.run(`CREATE TABLE IF NOT EXISTS trips (
+        rowid INTEGER PRIMARY KEY AUTOINCREMENT,
+        routeName TEXT NOT NULL,
+        routeStartingPoint TEXT NOT NULL,
+        region TEXT NOT NULL,
+        difficulty TEXT NOT NULL,
+        portageDistance INTEGER NOT NULL,
+        description TEXT NOT NULL,
+        image BLOB,
+        password TEXT NOT NULL
+    )`);
+}
