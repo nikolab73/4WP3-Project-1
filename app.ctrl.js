d661d48dccbc23d92a20db26fb8cd8eeaecc615a
Nikola Bogdanovic <bogdanon@mcmaster.ca>
2026-02-28 15:54:27 -0500

    rebuilding model file using source code as reference

--- app.ctrl.js
+++ app.ctrl.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
-module.exports = app;
+const mustacheExpress = require('mustache-express');
+
+app.engine("mustache", mustacheExpress());
+app.set("view engine", "mustache");
+app.set("views", "./views");
+
+const { getAllTrips, getTripById, tripsSortedByRegion } = require('./app.model.js');
